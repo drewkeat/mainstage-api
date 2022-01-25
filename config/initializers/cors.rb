a945c428395031240445e47ee13d4b71ace7f4da
Drew Keat <thedrewkeat@gmail.com>
2022-01-25 13:50:31 -0700

    Replace jwt secret
    -Add secret to rails credentials
    -Edit comments for deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # TODO: Change for deploy
+    # BEFOREDEPLOY: Change for deploy
     origins 'http://localhost:3000'
 
     resource '*',
