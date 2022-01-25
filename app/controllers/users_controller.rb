115858f620629e902f859be19b055beaef4ee250
Drew Keat <thedrewkeat@gmail.com>
2022-01-24 22:50:06 -0700

    Add UserSerializer in users_controller Create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     if @user.valid?
       @token = encode_token(user_id: @user.id)
-      render json: {user: @user, jwt: @token,}, status: :created
+      render json: {user: UserSerializer.new(@user), jwt: @token,}, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
