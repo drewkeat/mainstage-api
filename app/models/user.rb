6486060a42caab49bc9538dfcfc3fb64686dfbd5
Drew Keat <thedrewkeat@gmail.com>
2022-01-24 22:29:27 -0700

    Add validations and instance methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ApplicationRecord
   has_secure_password
+  before_save :normalize_names, :set_username
+  validates :email, uniqueness: { case_sensitive: false }
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+  
+
+  private
+  def set_username
+    self.username = self.username || self.first_name.downcase + self.last_name.downcase
+  end
+
+  def normalize_names
+    self.first_name = self.first_name.downcase.titleize
+    self.last_name = self.last_name.downcase.titleize
+  end
+
 end
