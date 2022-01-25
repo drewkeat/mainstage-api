6b3dc2fa47762c6a5683da6c90d0a5996b2546ad
Drew Keat <thedrewkeat@gmail.com>
2022-01-24 22:46:18 -0700

    Add jwt authorization logic to auth_controller and users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'auth/create'
+  get '/login', to: 'auth#create'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
