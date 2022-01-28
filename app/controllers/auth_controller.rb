22647166f097fe651370539baf3404a3a93b0625
Drew Keat <thedrewkeat@gmail.com>
2022-01-28 15:58:20 -0700

    Return jwt in headers and return UserSerializer in camel case

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,10 +4,8 @@ class AuthController < ApplicationController
     @user = User.find_by(email: user_login_params[:email])
     if @user && @user.authenticate(user_login_params[:password])
       token = encode_token({ user_id: @user.id })
-      render json: {
-        user: UserSerializer.new(@user),
-        jwt: token,
-      },
+      response.headers["jwt"] = token
+      render json: UserSerializer.new(@user),
       status: :accepted
     else
       render json: {
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,7 @@
 class UserSerializer
   include JSONAPI::Serializer
+  set_key_transform :camel_lower
   attributes :full_name, :first_name, :last_name, :email, :username
+
   
 end
