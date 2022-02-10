27ba540284223b8aae8fc0d2242560c41654f06e
Drew Keat <thedrewkeat@gmail.com>
2022-02-10 10:28:17 -0700

    Create 'auth#authenticate' action and route for user verification throughout client use

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :roles
   resources :productions
   post '/login', to: 'auth#create'
+  post '/authenticate', to: 'auth#authenticate'
 
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
