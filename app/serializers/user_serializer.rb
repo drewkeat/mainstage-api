6688a9f4ea72919e6fdb2efe995c088c029da919
Drew Keat <thedrewkeat@gmail.com>
2022-03-07 07:50:44 -0700

    update user_serializer to include 'managed productions' as 'productions' in renderd JSON

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,7 +3,7 @@ class UserSerializer
   set_key_transform :camel_lower
   attributes :full_name, :first_name, :last_name, :email, :username
   has_many :productions
-  has_many :managed_productions, serializer: :production, record_type: :managed_production
+  has_many :managed_productions, serializer: :production, record_type: :production
   has_many :roles
   has_many :applications
 end
